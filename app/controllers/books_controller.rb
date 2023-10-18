d9ea354e9195a4d470d7c58568986198f415d680
yuzuki00 <144969224+yuzuki00@users.noreply.github.com>
2023-10-18 14:35:21 +0000

    [Finish] destroy機能の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -35,6 +35,12 @@ class BooksController < ApplicationController
     end
   end
 
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
